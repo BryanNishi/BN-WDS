4d82a5126c0f135414bebdc06b06f42973edb768
Bryan Nishi <Bryan.Nishi@gmail.com>
2019-08-07 18:10:19 -0600

    Init

---
+++ app.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    $("a.nav-link").click(function (event) {
+        event.preventDefault();
+        $("html, body").animate({ scrollTop: $($(this).attr("href")).offset().top }, 500);
+    });
+});
+
+// When the user scrolls the page, execute function 
+window.onscroll = function () { scrollFunction() };
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function scrollFunction() {
+    if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+    } else {
+        navbar.classList.remove("sticky");
+    }
+}
